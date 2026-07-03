4c78742f870c9249e54672f6c64046b0af5edd88
Andrea Strömberg <andreastromberg@yahoo.se>
2026-07-03 16:39:52 +0200

    lagt tillbaka bakgrundsbild, ändrat mailadress i footer och kontaktsida

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
     <footer>
         <div class="footer__contact">
-            <a href="" class="footer__contact__email">lina@malm.se</a>
+            <a href="" class="footer__contact__email">linamalm55@gmail.com</a>
             <a href="https://www.instagram.com/linas_storytelling.and.art/" class="footer__contact__instagram">
                 <i class="fa-brands fa-instagram"></i>
             </a>
